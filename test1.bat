3c60a38c58bbec65d6e0345a9273bcb0830671f7
icedwizz <34060952+icedwizz@users.noreply.github.com>
2021-04-28 13:29:22 +0100

    Add files via upload

---
+++ test1.bat
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh devRelease.sh -apiKey=lFe6WXp.FyYoElioXvLfc3JG1DHp7k714JGqp98Jr2t -inputFile='CEVAPhase1 - PMS13 [0.0].xml' -serverBase=http://localhost:8088/semarchy -dataLocation=DemoTest -modelName=DemoTest -modelEdition=0.0
