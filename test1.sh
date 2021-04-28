a11c5e9b96397ff5d82b7bffc63d27fb1fe9dac4
icedwizz <34060952+icedwizz@users.noreply.github.com>
2021-04-28 16:47:12 +0100

    Add files via upload

---
+++ test1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh devRelease.sh -apiKey=ubXtYtO.fQN3Z20wIpZEluUGN1XmySrUbt1Tls9JLBX -inputFile='CEVAPhase1 [0.0].xml' -serverBase=http://localhost:8088/semarchy -dataLocation=DemoTest -modelName=DemoTest -modelEdition=0.0
